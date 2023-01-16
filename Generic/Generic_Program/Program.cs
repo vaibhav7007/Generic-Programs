9562199e42bf56305f01226cf021514de86ada79
vaibhav <vaibhavmehta524@gmail.com>
2023-01-16 10:36:50 +0530

    uc2 refactor

--- Generic/Generic_Program/Program.cs
+++ Generic/Generic_Program/Program.cs
@@ -8,7 +8,7 @@ namespace Generic_Programs
         {
             Console.WriteLine("Welcome to the Generics programs");
             Console.WriteLine("Choose the option from below");
-            Console.WriteLine("1.FindMaxInt\n2.FindFloat\n3.FindMaxString\n4.Refactor1");
+            Console.WriteLine("1.FindMaxInt\n2.FindFloat\n3.FindMaxString\n4.Refactor1\n5.Refactor2");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -33,6 +33,10 @@ namespace Generic_Programs
                     GenericRefactor1<int>.FindMax(100, 200, 500);
                     GenericRefactor1<float>.FindMax(10.5f, 20.7f, 56.8f);
                     GenericRefactor1<string>.FindMax("Apple", "Bannana", "Guava");
+                    break;
+                case 5:
+                    GenericsRefactor2<int> genericRefactor2 = new GenericsRefactor2<int>(12, 40, 60);
+                    genericRefactor2.TestMaximum();
                     break;
             }
 
