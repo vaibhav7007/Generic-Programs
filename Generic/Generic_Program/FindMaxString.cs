d0d6fda749b4e1e27a7dd39947f9f427e8fb7edf
vaibhav <vaibhavmehta524@gmail.com>
2023-01-16 10:29:38 +0530

    uc1 refactor

---
+++ Generic/Generic_Program/FindMaxString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Programs
+{
+    class FindMaxString
+    {
+        public static string FindString(string First_value, string Second_value, string Third_value)
+        {
+            if (First_value.CompareTo(Second_value) > 0 && First_value.CompareTo(Third_value) > 0)
+            {
+                Console.WriteLine("First string is Greater :" + First_value);
+                return First_value;
+            }
+            else if (Second_value.CompareTo(First_value) > 0 && Second_value.CompareTo(Third_value) > 0)
+            {
+                Console.WriteLine("Second string is Greater :" + Second_value);
+                return Second_value;
+            }
+            else
+            {
+                Console.WriteLine("Third string is Greater : " + Third_value);
+                return Third_value;
+            }
+        }
+    }
+}
---
+++ Generic/Generic_Program/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Generic_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Generics programs");
+            Console.WriteLine("Choose the option from below");
+            Console.WriteLine("1.FindMaxInt\n2.FindFloat\n3.FindMaxString\n4.Refactor1");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the first value");
+                    int First_val = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the second value");
+                    int Second_val = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the Three value");
+                    int Third_val = Convert.ToInt32(Console.ReadLine());
+                    FindMaxInt.FindIntMaxVal(First_val, Second_val, Third_val);
+                    break;
+
+                case 2:
+                    FindFloatMax.FindFloatMaxVal(12.3f, 44.5f, 55.6f);
+                    break;
+                case 3:
+                    FindMaxString.FindString("Apple", "Banana", "Guava");
+                    break;
+                case 4:
+                    GenericRefactor1<int>.FindMax(100, 200, 500);
+                    GenericRefactor1<float>.FindMax(10.5f, 20.7f, 56.8f);
+                    GenericRefactor1<string>.FindMax("Apple", "Bannana", "Guava");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
